c3ecd672ec86752e05288b9046dc03be3a3b9bf4
ElectricPotato <mironandrzej1@gmail.com>
2018-03-10 16:31:40 +0000

    plrInterfaces commented

--- plrInterface.py
+++ plrInterface.py
@@ -9,10 +9,7 @@ def getPlrX():
 def getPlrY():
 
 
-def getEnemyList()
-def enemyX(ID)
+def getEnemyList() #return list of enemy IDs
+def enemyX(ID) #x coord of a certain enemy
 def enemyY(ID)
 def enemyHP(ID)
-
-def absTurn(absAngle)
-def walkFwrd()
