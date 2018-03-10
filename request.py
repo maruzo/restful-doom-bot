7aa7635415c27769fad8fe250eef7fe7c4d6e206
maruzo <36563610+maruzo@users.noreply.github.com>
2018-03-10 15:40:41 +0000

    added post and corrections to request

--- request.py
+++ request.py
@@ -4,20 +4,33 @@ import json
 #port: 6001
 #url_extension: /api/world/objects
 #it returns the entire JSON
-def request(port, url_extension):
+def get(port, url_extension):
     url = "http://127.0.0.1:" + port
 
     r = requests.get(url + url_extension)
 
-    # Response, status etc
+    # Response and status
     if r.status_code == 200:
         data = r.json()
         return data
 
     return "ERROR"
 
+#port: 6001
+#url_extension: /api/world/objects
+#pyload: {'type': 'strafe-left','amount': 25}
+def post(port, url_extension, payload):
+    url = "http://127.0.0.1:" + port
+
+    r = requests.post(url + url_extension, data=json.dumps(payload))
+
+    # status
+    return r.status_code
+
+
 if __name__ == "__main__":
-    print(request("6001", "/api/world/objects"))
+    print(get("6001", "/api/world/objects"))
+    print(post("6001", "/api/player/actions", {'type': 'strafe-left','amount': 25}))
 
 
 
